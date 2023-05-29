107d0a26bb12ed4715621c4b224ab1783d265ab2
GavinZ5160 <gavin.z.5160@gmail.com>
2023-05-30 07:20:37 +0800

    update colorscheme name

--- init.lua
+++ init.lua
@@ -18,7 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+  colorscheme = "tokyonight-moon",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
